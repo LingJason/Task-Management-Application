368aece8d2b55338b4fc7ce2c31b267b83ecdaaa
Jason Ling <jasonling1996@gmail.com>
2023-05-04 14:26:08 -0700

    Update db

--- server/db/seeds/01_task.sql
+++ server/db/seeds/01_task.sql
@@ -2,8 +2,8 @@
 INSERT INTO task (task_name, task_owner_name, importance, due_date, notes)
   VALUES
   -- task seeds
-    ('Cook Rice', 'Jason', 'Medium','2023-04-29', 'Remember to wash it first'),
+    ('Cook Rice', 'Jason', 'Medium','2023-04-29', 'wash it first'),
     ('Take Dog Out', 'Chris', 'Low','2023-04-28', 'Only take him out if its sunny'),
     ('Laundry', 'Carol', 'High','2023-04-29', 'Seperate the whites and colors'),
-    ('Create a todo list Application', 'Jason', 'Low','2023-04-29', 'Remeber back to basics'),
+    ('Create a todo list Application', 'Jason', 'Low','2023-04-29', 'CRUD operations'),
     ('Car service', 'Monica', 'Medium','2023-04-29', '9:00 am sharp');
